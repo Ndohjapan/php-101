cdb860bceedec5ee072606f3e384f24932d58c33
Joel <ndohjoelmbj16@gmail.com>
2023-07-10 12:34:18 +0100

    samplesql connection

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -1,8 +1,8 @@
 <?php
-    define("DB_HOST", "localhost");
-    define("DB_USER", "joel");
-    define("DB_PASS", "123456");
-    define("DB_NAME", "php_dev");
+    define("DB_HOST", "us-cdbr-east-06.cleardb.net");
+    define("DB_USER", "b057e6b48643b4");
+    define("DB_PASS", "871c39da");
+    define("DB_NAME", "heroku_78b1e4d64a45816");
 
     // Create connection
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
